4f4390669ea9425e97e60fde0da14eed4fb5f0a3
alfred-mac <you@example.com>
2017-01-30 16:05:33 +0800

    init

---
+++ forestman/forestman/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  forestman
+//
+//  Created by alfred on 17/1/30.
+//  Copyright © 2017年 Alfred. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
